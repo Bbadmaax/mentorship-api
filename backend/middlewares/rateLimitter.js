77b2008a1d97ba2deeef0549442fff48312182a0
Bbadmaax <mustafjourney2025@gmail.com>
2025-11-25 17:38:55 +0300

    updated 2

--- backend/middlewares/rateLimitter.js
+++ backend/middlewares/rateLimitter.js
@@ -1,7 +1,7 @@
 import rateLimit from "express-rate-limit"
 
 export const limiter = rateLimit({
-    window : 15 * 60 * 1000,
+    windowMs : 15 * 60 * 1000,
     max : 100,
     message : "roo many request stopped"
 })
