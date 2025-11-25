32ca3d4388cf2568bde72e201ef8f32e7caac1b2
Bbadmaax <mustafjourney2025@gmail.com>
2025-11-25 19:08:27 +0300

    updated 8

--- backend/index.js
+++ backend/index.js
@@ -64,7 +64,7 @@ app.use(globalHandler)
 
 mongoose.connect(process.env.NODE_ENV == 'development' ? process.env.MONGO_URL_DEV : process.env.MONGO_URL_PRO)
 .then(()=> console.log('connected mongoose ✅'))
-.catch(()=> console.log("not connected ❌"))
+.catch(()=> console.log("not connected mongoose ❌"))
 
 app.listen(PORT, ()=> {
     console.log(`SERVER IS RUNNING http://localhost:${PORT}`)
